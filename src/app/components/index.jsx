ac3057bcc03c53e0c0958215caf4aff5ad10a3d4
Saurabh Singh <saurabh@91sqft.com>
2023-06-23 00:15:51 +0530

    Topic	: assignment complete Status	: done

---
+++ src/app/components/index.jsx
@@ -0,0 +1,35 @@
+import { Fragment } from "react";
+import "../globals.css";
+
+async function getData() {
+  const res = await fetch("https://fakestoreapi.com/products/");
+  const data = await res.json();
+  return data;
+}
+const PostScreen = async () => {
+  const product = await getData();
+  return (
+    <Fragment>
+      <div className="page-wrapper">
+        <span className="page-heading">Posts.js</span>
+        {product.map((item, index) => {
+          return (
+            // outer wrapper
+            <div className="outer-wrapper">
+              {/* inner containers */}
+              <img src={item.image} height={100} width={100} />
+              <div className="title-container">
+                {item.title}
+                <div className="description-container">{item.description}</div>
+              </div>
+
+              <div className="price-container">Rs {item.price}</div>
+            </div>
+          );
+        })}
+      </div>
+    </Fragment>
+  );
+};
+
+export default PostScreen;
---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+import PostScreen from "./components";
+
+export default function Home() {
+  return <PostScreen />;
+}
